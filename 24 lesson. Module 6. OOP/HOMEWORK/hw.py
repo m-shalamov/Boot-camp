e8a8157103d1ec79319ce06196fa6c7ec9c5ce5d
Michael Efimov <schumixer@list.ru>
2021-08-28 13:14:53 +0300

    lesson 24

---
+++ 24 lesson. Module 6. OOP/HOMEWORK/hw.py
@@ -0,0 +1,5 @@
+#Design a matrix computing class
+#Implement operations of addition of matrices and multiplication of all elements by some scalar value. Use the magic methods __ add__() and __ mul__() for this.
+#Use the __ add__() operation to add some integer value to all matrix elements
+#Ensure that the object is displayed correctly using the__str__() and __repr__() methods
+#Provide the ability to swap operands in the operations of addition and multiplication
