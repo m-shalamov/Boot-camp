0cd9d43dec7b7f5abf3013ab8e7ab95f466427e6
Michael Efimov <schumixer@list.ru>
2021-08-31 08:26:50 +0300

    lesson 22 fixed

--- 22 lesson. Module 11. Flask/code_examples/run.py
+++ 22 lesson. Module 11. Flask/code_examples/run.py
@@ -1,8 +1,4 @@
-# from flask import Flask, render_template, url_for, flash, redirect
-# from forms import RegistrationForm, LoginForm
-# from flask_sqlalchemy import SQLAlchemy
 from website import app
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
