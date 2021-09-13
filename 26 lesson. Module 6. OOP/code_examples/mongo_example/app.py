94aeda9d67b4042cc0fd875a6f28a67d88b3e3f6
Michael Efimov <schumixer@list.ru>
2021-09-13 19:58:50 +0300

    lesson_26_fixed

---
+++ 26 lesson. Module 6. OOP/code_examples/mongo_example/app.py
@@ -0,0 +1,25 @@
+import data.mongo_setup as mongo_setup
+import app_hosts
+import app_guests
+
+def main():
+    mongo_setup.global_init()
+    try:
+        while True:
+            if user_choice() == "g":
+                app_guests.run()
+            else:
+                app_hosts.run()
+    except KeyboardInterrupt:
+        return
+
+def user_choice():
+    print("type [g] - for guests")
+    print("type [h] - for hosts")
+    print()
+    choice = input()
+    return choice
+
+if __name__ == "__main__":
+    main()
+        
